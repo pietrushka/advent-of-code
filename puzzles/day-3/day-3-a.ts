45a942f7dd2b4852f0f12d2094e316bb28435820
pietrushka <piotrekwisnia18@gmail.com>
2023-12-04 12:37:59 +0100

    Day 3

--- puzzles/day-3/day-3-a.ts
+++ puzzles/day-3/day-3-a.ts
@@ -1,10 +1,88 @@
-import { readData } from '../../shared.ts';
-import chalk from 'chalk';
+import { readData } from '../../shared.ts'
+import chalk from 'chalk'
 
-export async function day3a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+function sumNumArray(nums: number[]) {
+	return nums.reduce((acc, x) => acc + x, 0)
 }
 
-const answer = await day3a();
-console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+function getChar(x: number, y: number, data: string[]): string | undefined {
+	return data?.[y]?.[x]
+}
+
+function checkSymbolNerby(x: number, y: number, data: string[]) {
+	const coordinates = [
+		[x, y - 1],
+		[x + 1, y - 1],
+		[x + 1, y],
+		[x + 1, y + 1],
+		[x, y + 1],
+		[x - 1, y + 1],
+		[x - 1, y],
+		[x - 1, y - 1],
+	]
+
+	return coordinates.some(([positionX, positionY]) => {
+		const char = getChar(positionX, positionY, data)
+		const isCharSymbol = /[^\w\s.]/.test(char)
+		return isCharSymbol
+	})
+}
+
+function validateNumber(
+	processedNumberCharIndexes: number[],
+	lineIdx: number,
+	data: string[]
+) {
+	return processedNumberCharIndexes
+		.map((x) => [x, lineIdx])
+		.some((coordinates) =>
+			checkSymbolNerby(coordinates[0], coordinates[1], data)
+		)
+}
+
+export async function day3a(dataPath?: string) {
+	const data = await readData(dataPath)
+
+	let validNumbers = []
+	for (let lineIdx = 0; lineIdx < data.length; lineIdx++) {
+		const line = data[lineIdx]
+
+		let processedNumberCharIndexes: number[] = []
+
+		for (let charIdx = 0; charIdx <= line.length; charIdx++) {
+			const char = getChar(charIdx, lineIdx, data)
+			const isCharNumber = /[0-9]/.test(char)
+
+			if (isCharNumber) {
+				processedNumberCharIndexes.push(charIdx)
+				continue
+			}
+
+			if (!processedNumberCharIndexes.length) {
+				continue
+			}
+
+			const isNumberValid = validateNumber(
+				processedNumberCharIndexes,
+				lineIdx,
+				data
+			)
+
+			if (isNumberValid) {
+				const number = parseInt(
+					processedNumberCharIndexes.map((idx) => line[idx]).join('')
+				)
+				validNumbers.push(number)
+			}
+
+			processedNumberCharIndexes = []
+		}
+	}
+
+	return sumNumArray(validNumbers)
+}
+
+console.time('test')
+const answer = await day3a()
+console.timeEnd('test')
+console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer))
--- puzzles/day-3/day-3-b.ts
+++ puzzles/day-3/day-3-b.ts
@@ -1,10 +1,100 @@
-import { readData } from '../../shared.ts';
-import chalk from 'chalk';
+import { readData } from '../../shared.ts'
+import chalk from 'chalk'
 
-export async function day3b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+function sumNumArray(nums: number[]) {
+	return nums.reduce((acc, x) => acc + x, 0)
 }
 
-const answer = await day3b();
-console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+const ADJACENT_NUMBERS_REQUIRED = 2
+
+function getChar(x: number, y: number, data: string[]): string | undefined {
+	return data?.[y]?.[x]
+}
+
+function getAdjacentPositions(x: number, y: number) {
+	return [
+		[x, y - 1],
+		[x + 1, y - 1],
+		[x + 1, y],
+		[x + 1, y + 1],
+		[x, y + 1],
+		[x - 1, y + 1],
+		[x - 1, y],
+		[x - 1, y - 1],
+	]
+}
+
+function getNumbersCoordinateStrings(data: string[]) {
+	let numbersCoordinates: string[][] = []
+
+	for (let lineIdx = 0; lineIdx < data.length; lineIdx++) {
+		const line = data[lineIdx]
+
+		let processedNumberCharIndexes: string[] = []
+
+		for (let charIdx = 0; charIdx <= line.length; charIdx++) {
+			const char = getChar(charIdx, lineIdx, data)
+			const isCharNumber = /[0-9]/.test(char)
+
+			if (isCharNumber) {
+				processedNumberCharIndexes.push([charIdx, lineIdx].join(';'))
+				continue
+			}
+
+			if (!processedNumberCharIndexes.length) {
+				continue
+			}
+
+			numbersCoordinates.push(processedNumberCharIndexes)
+
+			processedNumberCharIndexes = []
+		}
+	}
+
+	return numbersCoordinates // eg. [["0;1", 0;2"]]
+}
+
+function convertCoordinatesStringsToNumber(
+	coordinatesStrings: string[],
+	data: string[]
+) {
+	const numberDigits = coordinatesStrings.map((coords) => {
+		const [x, y] = coords.split(';').map(Number)
+		return getChar(x, y, data)
+	})
+
+	return parseInt(numberDigits.join(''))
+}
+
+export async function day3b(dataPath?: string) {
+	const data = await readData(dataPath)
+	const numbersCoordinates = getNumbersCoordinateStrings(data)
+
+	const ratios = data.flatMap((line, lineIdx) =>
+		Array.from(line).flatMap((char, charIdx) => {
+			if (char !== '*') return []
+
+			const adjacentCoordinates = getAdjacentPositions(charIdx, lineIdx).map(
+				(coords) => coords.join(';')
+			)
+			const adjacentNumbers = numbersCoordinates.filter((coords) =>
+				coords.some((coord) => adjacentCoordinates.includes(coord))
+			)
+
+			if (adjacentNumbers.length !== ADJACENT_NUMBERS_REQUIRED) {
+				return []
+			}
+
+			const ratio =
+				convertCoordinatesStringsToNumber(adjacentNumbers[0], data) *
+				convertCoordinatesStringsToNumber(adjacentNumbers[1], data)
+
+			return ratio
+		})
+	)
+
+	return sumNumArray(ratios)
+}
+
+const answer = await day3b()
+console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer))
