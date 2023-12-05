5af451f77ddc79b73a3e9479b4d5758badc82a74
pietrushka <piotrekwisnia18@gmail.com>
2023-12-05 10:43:53 +0100

    Day 4

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -1,10 +1,47 @@
-import { readData } from '../../shared.ts';
-import chalk from 'chalk';
+import { readData } from '../../shared.ts'
+import chalk from 'chalk'
 
-export async function day4a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+function sumNumArray(nums: number[]) {
+	return nums.reduce((acc, x) => acc + x, 0)
 }
 
-const answer = await day4a();
-console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+function parseNumberPart(numString: string) {
+	return numString.split(' ').flatMap((x) => (x ? [Number(x)] : []))
+}
+
+function parseLine(line: string) {
+	const [winningNumsText, myNumsText] = line.split(': ')[1].split(' | ')
+	return {
+		winningNums: parseNumberPart(winningNumsText),
+		myNums: parseNumberPart(myNumsText),
+	}
+}
+
+function calcPoints(matchCount: number) {
+	if (matchCount === 0) return 0
+	let result = 1
+	for (let i = 1; i < matchCount; i++) {
+		result = result * 2
+	}
+	return result
+}
+
+function getCardPoints(line: string) {
+	const { winningNums, myNums } = parseLine(line)
+	const matchCount = winningNums.reduce((acc, winningNum) => {
+		if (myNums.includes(winningNum)) {
+			return acc + 1
+		}
+		return acc
+	}, 0)
+	return calcPoints(matchCount)
+}
+
+export async function day4a(dataPath?: string) {
+	const data = await readData(dataPath)
+	const cardsPoints = data.map(getCardPoints)
+	return sumNumArray(cardsPoints)
+}
+
+const answer = await day4a()
+console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer))
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -1,10 +1,47 @@
-import { readData } from '../../shared.ts';
-import chalk from 'chalk';
+import { readData } from '../../shared.ts'
+import chalk from 'chalk'
 
-export async function day4b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+function sumNumArray(nums: number[]) {
+	return nums.reduce((acc, x) => acc + x, 0)
 }
 
-const answer = await day4b();
-console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+function parseNumberPart(numString) {
+	return numString.split(' ').flatMap((x) => (x ? [Number(x)] : []))
+}
+
+function parseLine(line) {
+	const [winningNumsText, myNumsText] = line
+		.split('|')
+		.map((part) => part.trim())
+	return {
+		winningNums: parseNumberPart(winningNumsText),
+		myNums: parseNumberPart(myNumsText),
+	}
+}
+
+function getCardMatchCount(line) {
+	const { winningNums, myNums } = parseLine(line)
+	return winningNums.reduce(
+		(acc, winningNum) => (myNums.includes(winningNum) ? acc + 1 : acc),
+		0
+	)
+}
+
+export async function day4b(dataPath?: string) {
+	const cards = await readData(dataPath)
+
+	const copies = Array(cards.length).fill(1)
+
+	for (let [cardIdx, card] of cards.entries()) {
+		const matchCount = getCardMatchCount(card)
+
+		for (let i = 0; i < matchCount; i++) {
+			copies[cardIdx + i + 1] += copies[cardIdx]
+		}
+	}
+
+	return sumNumArray(copies)
+}
+
+const answer = await day4b()
+console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer))
