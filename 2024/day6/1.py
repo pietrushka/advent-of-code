99fd8ad82c567206f06284bbe0de2fcb5b68d9aa
pietrushka <piotrekwisnia18@gmail.com>
2025-10-21 23:14:27 +0200

    2024 day 6 part 2

--- 2024/day6/1.py
+++ 2024/day6/1.py
@@ -1,5 +1,6 @@
 import math
 import sys
+
 print(sys.getrecursionlimit())
 
 f = open("day6/data.txt", "r")
@@ -9,58 +10,63 @@ sys.setrecursionlimit(5000)
 
 map_dict = {}
 initial_current_pos = None
-initial_direction = 'up'
+initial_direction = "up"
 path = set()
 
 for iy, row in enumerate(data.split("\n")):
-    for ix, cell in enumerate(row): 
-        if cell == '^':
+    for ix, cell in enumerate(row):
+        if cell == "^":
             initial_current_pos = (ix, iy)
             path.add(initial_current_pos)
         map_dict[(ix, iy)] = cell
 
 size = math.sqrt(len(map_dict))  # Assume square map
 
+print(map_dict)
+
+
 def get_next_pos(current_pos, direction):
-    if direction == 'up':
+    if direction == "up":
         return (current_pos[0], current_pos[1] - 1)
-    elif direction == 'down':
+    elif direction == "down":
         return (current_pos[0], current_pos[1] + 1)
-    elif direction == 'left':
+    elif direction == "left":
         return (current_pos[0] - 1, current_pos[1])
-    elif direction == 'right':
+    elif direction == "right":
         return (current_pos[0] + 1, current_pos[1])
     else:
-        raise Exception('Invalid direction')
+        raise Exception("Invalid direction")
+
 
 def check_find_exit(current_pos, direction):
     (x, y) = current_pos
 
-    if direction == 'up' and y == 0:
+    if direction == "up" and y == 0:
         return True
-    elif direction == 'down' and y == size - 1:
+    elif direction == "down" and y == size - 1:
         return True
-    elif direction == 'left' and x == 0:
+    elif direction == "left" and x == 0:
         return True
-    elif direction == 'right' and x == size - 1:
+    elif direction == "right" and x == size - 1:
         return True
 
     return False
-    
+
+
 def get_next_direction(direction):
-    directions = ['up', 'right', 'down', 'left']
+    directions = ["up", "right", "down", "left"]
     idx = directions.index(direction)
     idx += 1
     if idx == len(directions):
         idx = 0
     return directions[idx]
 
+
 def make_move(current_pos, direction):
-    print('make_move', current_pos, direction)
     if check_find_exit(current_pos, direction):
         return
     next_pos = get_next_pos(current_pos=current_pos, direction=direction)
-    is_next_pos_free = map_dict[next_pos] != '#'
+    is_next_pos_free = map_dict[next_pos] != "#"
 
     if is_next_pos_free:
         current_pos = next_pos
@@ -69,6 +75,7 @@ def make_move(current_pos, direction):
 
     new_direction = get_next_direction(direction=direction)
     return make_move(current_pos, new_direction)
+
 
 make_move(initial_current_pos, initial_direction)
 print(len(path))
--- 2024/day6/2.py
+++ 2024/day6/2.py
@@ -1,110 +1,108 @@
 import math
 import sys
+
+# print(sys.getrecursionlimit())
+
+f = open("day6/data'.txt", "r")
+data = f.read()
 sys.setrecursionlimit(10000)
 
-f = open("day6/data.txt", "r")
-data = f.read()
 
-
-map_dict = {}
-initial_pos = None
-initial_direction = 'up'
-path = set()
-path_arr = []
-possible_loops = set()
+initial_map_dict = {}
+initial_current_pos = None
+initial_direction = "up"
 
 for iy, row in enumerate(data.split("\n")):
-    for ix, cell in enumerate(row): 
-        if cell == '^':
-            initial_pos = (ix, iy)
-            path.add((ix, iy, initial_direction))
-            path_arr.append((ix, iy, initial_direction))
-        map_dict[(ix, iy)] = cell
+    for ix, cell in enumerate(row):
+        if cell == "^":
+            initial_current_pos = (ix, iy)
+        initial_map_dict[(ix, iy)] = cell
 
-size = math.sqrt(len(map_dict))  # Assume square map
+size = math.sqrt(len(initial_map_dict))  # Assume square map
+
 
 def get_next_pos(current_pos, direction):
-    if direction == 'up':
+    if direction == "up":
         return (current_pos[0], current_pos[1] - 1)
-    elif direction == 'down':
+    elif direction == "down":
         return (current_pos[0], current_pos[1] + 1)
-    elif direction == 'left':
+    elif direction == "left":
         return (current_pos[0] - 1, current_pos[1])
-    elif direction == 'right':
+    elif direction == "right":
         return (current_pos[0] + 1, current_pos[1])
     else:
-        raise Exception('Invalid direction')
+        raise Exception("Invalid direction")
 
-def check_find_exit(current_pos, direction):
-    (x, y) = current_pos
 
-    if direction == 'up' and y == 0:
-        return True
-    elif direction == 'down' and y == size - 1:
-        return True
-    elif direction == 'left' and x == 0:
-        return True
-    elif direction == 'right' and x == size - 1:
-        return True
-
-    return False
-    
 def get_next_direction(direction):
-    directions = ['up', 'right', 'down', 'left']
+    directions = ["up", "right", "down", "left"]
     idx = directions.index(direction)
     idx += 1
     if idx == len(directions):
         idx = 0
     return directions[idx]
 
-def check_loop(current_pos, direction):
-    temp_path = path.copy()
-    next_direction = get_next_direction(direction=direction)
-    block_position = get_next_pos(current_pos=current_pos, direction=direction)
-    is_loop = simulate_moves(current_pos=current_pos, direction=next_direction, temp_path=temp_path, block_position=block_position)
-    return is_loop
 
-def simulate_moves(current_pos, direction, temp_path, block_position):
+def check_find_exit(current_pos, direction):
+    (x, y) = current_pos
+
+    if direction == "up" and y == 0:
+        return True
+    elif direction == "down" and y == size - 1:
+        return True
+    elif direction == "left" and x == 0:
+        return True
+    elif direction == "right" and x == size - 1:
+        return True
+
+    return False
+
+
+def simulate_path(map_dict, current_pos, direction, path):
     if check_find_exit(current_pos, direction):
         return False
 
     next_pos = get_next_pos(current_pos=current_pos, direction=direction)
-
-    if (next_pos[0], next_pos[1], direction) in temp_path:
-        return True
-    
-    is_next_pos_free = map_dict[next_pos] != '#' and next_pos != block_position
+    is_next_pos_free = map_dict[next_pos] != "#"
 
     if is_next_pos_free:
-        current_pos = next_pos
-        temp_path.add((next_pos[0], next_pos[1], direction))
-        return simulate_moves(current_pos=current_pos, direction=direction, temp_path=temp_path, block_position=block_position)
+        if (next_pos, direction) in path:
+            return True
+        path.add((next_pos, direction))
+        return simulate_path(map_dict, next_pos, direction, path)
 
     new_direction = get_next_direction(direction=direction)
-    temp_path.add((current_pos[0], current_pos[1], new_direction))
+    return simulate_path(map_dict, current_pos, new_direction, path)
 
-    return simulate_moves(current_pos, new_direction, temp_path, block_position=block_position)
 
-def make_move(current_pos, direction):
-    if check_find_exit(current_pos, direction):
-        return
-    next_pos = get_next_pos(current_pos=current_pos, direction=direction)
-    is_next_pos_free = map_dict[next_pos] != '#'
+total = 0
+total_length = len(initial_map_dict.items())
+idx = 1
 
-    if is_next_pos_free:
-        if len(path) > 1 and check_loop(current_pos=current_pos, direction=direction):
-            possible_loops.add(next_pos)
-        current_pos = next_pos
-        path.add((next_pos[0], next_pos[1], direction))
-        path_arr.append((next_pos[0], next_pos[1], direction))
-        return make_move(current_pos, direction)
+for item in list(initial_map_dict.items()):
+    print(f"{idx}/{total_length}")
+    map_dict_copy = initial_map_dict.copy()
+    item_value = item[1]
+    if item_value == "#" or item_value == "^":
+        continue
 
-    new_direction = get_next_direction(direction=direction)
-    path.add((current_pos[0], current_pos[1], new_direction))
-    path_arr.append((current_pos[0], current_pos[1], new_direction))
+    item_x = item[0][0]
+    item_y = item[0][1]
 
-    return make_move(current_pos, new_direction)
+    map_dict_copy[item_x, item_y] = "#"
 
-make_move(initial_pos, initial_direction)
-print(possible_loops)
-print(len(possible_loops))
+    path = set()
+
+    result = simulate_path(
+        map_dict_copy,
+        current_pos=initial_current_pos,
+        direction=initial_direction,
+        path=path,
+    )
+
+    if result:
+        total += 1
+
+    idx += 1
+
+print(total)
